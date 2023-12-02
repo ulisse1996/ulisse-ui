9d8d05dc9cf0ee97eab1c8853cbc3fe748217715
Donato Leone <donato.leone@edreamsodigeo.com>
2023-12-02 19:39:33 +0100

    Add missing react-aria

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: ['../lib/**/*.mdx', '../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -14,16 +13,6 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
-  },
-  viteFinal: async (c) => {
-    return mergeConfig(c, {
-      build: {
-        rollupOptions: {
-          input: ['lib/index.ts'],
-          external: [],
-        },
-      },
-    });
   },
 };
 export default config;
