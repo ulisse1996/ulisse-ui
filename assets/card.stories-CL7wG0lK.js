dac5cd0af915452a7f1211c2a8d20f2c74cb2478
ulisse1996 <ulissis1996@gmail.com>
2023-12-03 12:37:48 +0000

    Deploying to gh-pages from @ ulisse1996/ulisse-ui@b2718a60e3c24c84374c46d782a8faecb9ba7e4d 🚀

--- assets/card.stories-CL7wG0lK.js
+++ assets/card.stories-CL7wG0lK.js
@@ -1,4 +1,4 @@
-import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{I as b}from"./chunk-W7EQS7E7-E5h29MIc.js";import{B as f}from"./index-iptAJApz.js";import{r as a}from"./index-4g5l5LRQ.js";import{a as i}from"./utils-LqYgCrNY.js";import"./index-v55UWy9B.js";import"./spinner-dOrrrPEq.js";import"./_commonjsHelpers-4gQjN7DL.js";const r=a.forwardRef(({className:t,...n},s)=>e.jsx("div",{ref:s,className:i("rounded-lg border bg-card text-card-foreground shadow-sm",t),...n}));r.displayName="Card";const C=a.forwardRef(({className:t,...n},s)=>e.jsx("div",{ref:s,className:i("flex flex-col space-y-1.5 p-6",t),...n}));C.displayName="CardHeader";const x=a.forwardRef(({className:t,children:n,...s},w)=>e.jsx("h3",{ref:w,className:i("text-2xl font-semibold leading-none tracking-tight",t),...s,children:n}));x.displayName="CardTitle";const k=a.forwardRef(({className:t,...n},s)=>e.jsx("p",{ref:s,className:i("text-sm text-muted-foreground",t),...n}));k.displayName="CardDescription";const l=a.forwardRef(({className:t,...n},s)=>e.jsx("div",{ref:s,className:i("p-6 pt-0",t),...n}));l.displayName="CardContent";const m=a.forwardRef(({className:t,...n},s)=>e.jsx("div",{ref:s,className:i("flex items-center p-6 pt-0",t),...n}));m.displayName="CardFooter";try{r.displayName="Card",r.__docgenInfo={description:"",displayName:"Card",props:{}}}catch{}const D={component:r,title:"Components/Card"},o={render:()=>e.jsx("div",{className:"p-24",children:e.jsxs(r,{children:[e.jsxs(C,{children:[e.jsx(x,{children:"My Card Title"}),e.jsx(k,{children:"Subtitle card"})]}),e.jsx(l,{children:`Lorem Ipsum is simply dummy text of the printing and typesetting
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{I as b}from"./chunk-W7EQS7E7-E5h29MIc.js";import{B as f}from"./index-iptAJApz.js";import{r as a}from"./index-4g5l5LRQ.js";import{a as i}from"./utils-LqYgCrNY.js";import"./index-v55UWy9B.js";import"./spinner-dOrrrPEq.js";import"./_commonjsHelpers-4gQjN7DL.js";const r=a.forwardRef(({className:t,...n},s)=>e.jsx("div",{ref:s,className:i("rounded-lg border bg-card text-card-foreground shadow-sm",t),...n}));r.displayName="Card";const C=a.forwardRef(({className:t,...n},s)=>e.jsx("div",{ref:s,className:i("flex flex-col space-y-1.5 p-6",t),...n}));C.displayName="CardHeader";const x=a.forwardRef(({className:t,children:n,...s},k)=>e.jsx("h3",{ref:k,className:i("text-2xl font-semibold leading-none tracking-tight",t),...s,children:n}));x.displayName="CardTitle";const w=a.forwardRef(({className:t,...n},s)=>e.jsx("p",{ref:s,className:i("text-sm text-muted-foreground",t),...n}));w.displayName="CardDescription";const m=a.forwardRef(({className:t,...n},s)=>e.jsx("div",{ref:s,className:i("p-6 pt-0",t),...n}));m.displayName="CardContent";const l=a.forwardRef(({className:t,...n},s)=>e.jsx("div",{ref:s,className:i("flex items-center p-6 pt-0",t),...n}));l.displayName="CardFooter";try{r.displayName="Card",r.__docgenInfo={description:"",displayName:"Card",props:{}}}catch{}const D={component:r,title:"Components/Card"},o={render:()=>e.jsx("div",{className:"max-w-md p-4",children:e.jsxs(r,{children:[e.jsxs(C,{children:[e.jsx(x,{children:"My Card Title"}),e.jsx(w,{children:"Subtitle card"})]}),e.jsx(m,{children:`Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
@@ -6,7 +6,7 @@ import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{I as b}from"./chunk-W7EQS7E
             remaining essentially unchanged. It was popularised in the 1960s
             with the release of Letraset sheets containing Lorem Ipsum passages,
             and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.`}),e.jsx(m,{children:e.jsx(f,{children:"Click me pls"})})]})})},d={render:()=>e.jsx("div",{className:"p-24",children:e.jsxs(r,{children:[e.jsx(b,{className:"mb-6 rounded-t-lg border-t",layout:"constrained",width:600,height:400,src:"https://cdn.shopify.com/static/sample-images/bath.jpeg"}),e.jsx(l,{children:`Lorem Ipsum is simply dummy text of the printing and typesetting
+            PageMaker including versions of Lorem Ipsum.`}),e.jsx(l,{children:e.jsx(f,{children:"Click me pls"})})]})})},d={render:()=>e.jsx("div",{className:"max-w-md p-4",children:e.jsxs(r,{children:[e.jsx(b,{className:"mb-6 rounded-t-lg border-t",layout:"constrained",width:600,height:400,src:"https://cdn.shopify.com/static/sample-images/bath.jpeg"}),e.jsx(m,{children:`Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
@@ -14,9 +14,9 @@ import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{I as b}from"./chunk-W7EQS7E
             remaining essentially unchanged. It was popularised in the 1960s
             with the release of Letraset sheets containing Lorem Ipsum passages,
             and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.`}),e.jsx(m,{children:e.jsx(f,{children:"Click me pls"})})]})})};var p,c,u;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+            PageMaker including versions of Lorem Ipsum.`}),e.jsx(l,{children:e.jsx(f,{children:"Click me pls"})})]})})};var p,c,u;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
   render: () => {
-    return <div className="p-24">
+    return <div className="max-w-md p-4">
         <Card>
           <CardHeader>
             <CardTitle>My Card Title</CardTitle>
@@ -41,7 +41,7 @@ import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{I as b}from"./chunk-W7EQS7E
   }
 }`,...(u=(c=o.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var h,y,g;d.parameters={...d.parameters,docs:{...(h=d.parameters)==null?void 0:h.docs,source:{originalSource:`{
   render: () => {
-    return <div className="p-24">
+    return <div className="max-w-md p-4">
         <Card>
           <Image className="mb-6 rounded-t-lg border-t" layout="constrained" width={600} height={400} src="https://cdn.shopify.com/static/sample-images/bath.jpeg" />
           <CardContent>
