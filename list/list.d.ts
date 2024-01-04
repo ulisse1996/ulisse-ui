c505205c3623125fcc11a2f58d4855f1ddef5602
ulisse1996 <ulissis1996@gmail.com>
2024-01-04 14:46:55 +0000

    Deploying to gh-pages from @ ulisse1996/ulisse-ui@29fe9765936ca7aa929e59eb5cf3444a2419915c 🚀

--- list/list.d.ts
+++ list/list.d.ts
@@ -1,8 +1,15 @@
-import type { ButtonHTMLAttributes, HTMLAttributes } from 'react';
+import type { ButtonHTMLAttributes, ElementType, ForwardedRef, ForwardRefExoticComponent, HTMLAttributes } from 'react';
+import type { PolymorphicProps } from '../types';
 interface ListProps extends HTMLAttributes<HTMLDetailsElement> {
 }
-interface ListItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-}
-declare const List: import("react").ForwardRefExoticComponent<ListProps & import("react").RefAttributes<HTMLDetailsElement>>;
-declare const ListItem: import("react").ForwardRefExoticComponent<ListItemProps & import("react").RefAttributes<HTMLButtonElement>>;
+type ListItemProps<E extends ElementType = 'button'> = PolymorphicProps<E> & ButtonHTMLAttributes<HTMLButtonElement>;
+declare const List: ForwardRefExoticComponent<ListProps & import("react").RefAttributes<HTMLDetailsElement>>;
+declare function InternalListItem<T extends ElementType = 'button'>({ className, as, ...props }: ListItemProps<T>, ref: ForwardedRef<HTMLButtonElement>): import("react/jsx-runtime").JSX.Element;
+declare const ListItem: <T extends ElementType>(props: import("react").PropsWithoutRef<import("react").ComponentProps<T>> & {
+    as?: T | undefined;
+} & {
+    children?: import("react").ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement> & {
+    ref?: ForwardedRef<HTMLButtonElement> | undefined;
+}) => ReturnType<typeof InternalListItem>;
 export { List, ListItem, type ListItemProps, type ListProps };
